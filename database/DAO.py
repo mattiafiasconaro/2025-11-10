3b69221d161b7e24f9d51630c7ebfc942365da97
Giuseppe Averta <g.averta3@gmail.com>
2025-11-09 14:57:28 +0100

    first commit

---
+++ database/DAO.py
@@ -0,0 +1,23 @@
+from database.DB_connect import DBConnect
+
+from model.store import Store
+
+
+class DAO():
+    @staticmethod
+    def getAllStores():
+        conn = DBConnect.get_connection()
+
+        results = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = "SELECT * from stores"
+
+        cursor.execute(query)
+
+        for row in cursor:
+            results.append(Store(**row))
+
+        cursor.close()
+        conn.close()
+        return results
